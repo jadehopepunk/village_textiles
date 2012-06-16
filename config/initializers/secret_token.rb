c2eae93de8e6dbbf13dc9635de45a826e49de755
Craig Ambrose <craig@craigambrose.com>
2012-06-16 15:43:35 +1200

    First commit, empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VillageTextiles::Application.config.secret_token = '10cadcbf5c9b1accc8ac6717358ecd08f07e15722fd1affff67408548fd54fc025320135a479a2e31743d5177fed564533914a908f3737c973ba53d76c742fbe'
